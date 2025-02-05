ece04678585a790fafa30737281e12f1511c70f4
TranTrungNghiaDev <trantrungnghiadev1998@gmail.com>
2025-02-05 11:17:11 +0700

    Complete basic logic
    Complete basic logic

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const indexRouter = require("./routers/indexRouter");
-const newRouter = require("./routers/newRouter");
 const path = require("path");
 
 const app = express();
@@ -8,8 +7,9 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+
 app.use("/", indexRouter);
-app.use("/new", newRouter);
 
 const PORT = 3000;
 
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -7,17 +7,35 @@ const messages = [
     {
         text: "Hi there!",
         user: "Nghia",
-        added: new Date()
+        added: new Date().toISOString().split('T')[0]
     },
     {
         text: "I love you",
         user: "Ngan",
-        added: new Date()
+        added: new Date().toISOString().split('T')[0]
     }
 ];
 
 indexRouter.get("/", (req, res) => {
     res.render("index", {title: "Mini Message", messages: messages});
+})
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form/form");
+})
+
+indexRouter.post("/new", (req, res) => {
+    messages.push({text: req.body.message, user: req.body.author, added: new Date().toISOString().split('T')[0]});
+    res.redirect("/");
+})
+
+indexRouter.post("/detail", (req, res) => {
+    const message = {};
+    message.user = req.body.user;
+    message.text = req.body.message;
+    message.added = req.body.added;
+
+    res.render("MessageDetail/messageDetail", {message: message});
 })
 
 module.exports = indexRouter;
